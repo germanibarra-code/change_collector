4c514a61b42a22fe677fd75dda7c5b4990f8ffa1
EmilianoSP9033 <AL03098431@tecmilenio.mx>
2026-01-16 16:35:04 -0600

    nueva rama des

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -213,7 +213,7 @@ class _LoginScreenState extends State<LoginScreen> {
                       }
                       return null;
                     },
-                  ), //lira
+                  ), //desa
                   const SizedBox(height: 10),
 
                   // Olvidaste tu contraseña
